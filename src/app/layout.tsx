a8bc73c3d03d0b172cab39fcb0134763c39b4b5c
ZchMac2020M1 <571928489@qq.com>
2024-12-11 14:14:18 +0800

    refactor(layout): 注释掉字体相关代码
    - 注释掉了 layout.tsx 文件中的 localFont 导入和使用
    - 移除了 body 元素中的字体相关 class
    - 保留了 antialiased 类以保持文本抗锯齿效果

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+// import localFont from 'next/font/local'
 import './globals.css'
 import { ThemeProvider } from 'next-themes'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
+// const geistSans = localFont({
+//   src: './fonts/GeistVF.woff',
+//   variable: '--font-geist-sans',
+//   weight: '100 900',
+// })
+// const geistMono = localFont({
+//   src: './fonts/GeistMonoVF.woff',
+//   variable: '--font-geist-mono',
+//   weight: '100 900',
+// })
 
 export const metadata: Metadata = {
   title: 'Z.ToolBox',
@@ -34,9 +34,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      {/* className={`${geistSans.variable} ${geistMono.variable} antialiased`} */}
+      <body className={'antialiased'}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
